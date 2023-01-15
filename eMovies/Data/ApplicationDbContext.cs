06bea74fb2dc1a05d6595a6d961f01cdc41ec56f
kaltrina-morina <km51097@ubt-uni.net>
2023-01-15 17:01:10 +0100

    Movie

--- eMovies/Data/ApplicationDbContext.cs
+++ eMovies/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using eMovies.Models;
 
 namespace eMovies.Data
 {
@@ -12,5 +13,6 @@ namespace eMovies.Data
             : base(options)
         {
         }
+        public DbSet<eMovies.Models.Movie> Movie { get; set; }
     }
 }
