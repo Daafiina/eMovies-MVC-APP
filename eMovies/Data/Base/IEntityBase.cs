2cdee3821de84d5404bf2046ef96fae4ba364f41
Daafiina <da52811@ubt-uni.net>
2023-01-18 01:18:05 +0100

    Added Base folder

---
+++ eMovies/Data/Base/IEntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace eMovies.Data.Base
+{
+    public interface IEntityBase //E krijojme kete ne menyre qe mos me kriju interface per krejt modelet ne veti,Generic solution
+    {
+        int Id { get; set; }
+    }
+}
